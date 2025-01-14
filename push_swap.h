c40060c4644c380e25c78a78062a4890a22edfa6
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 18:44:27 +0000

    Remove declarations of static functions from push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -4,19 +4,19 @@
 # include "libft/libft.h"
 # include <limits.h>
 
-void	exit_with_error(t_list **stack_a, t_list **stack_b);
-int		is_valid_integer(const char *str, long long *num);
-int		contains_duplicate(t_list *stack, int num);
 void	push_element(t_list **dest_stack, t_list **src_stack);
 void	push_to_a(t_list **stack_a, t_list **stack_b);
 void	push_to_b(t_list **stack_a, t_list **stack_b);
+
 void	swap_top_two_elements(t_list **stack);
 void	swap_a(t_list **stack_a);
 void	swap_b(t_list **stack_b);
+
 void	rotate_stack_up(t_list **stack);
 void	rotate_a(t_list **stack_a);
 void	rotate_b(t_list **stack_b);
 void	rotate_a_b(t_list **stack_a, t_list **stack_b);
+
 void	reverse_rotate_stack_down(t_list **stack);
 void	reverse_rotate_a(t_list **stack_a);
 void	reverse_rotate_b(t_list **stack_b);
