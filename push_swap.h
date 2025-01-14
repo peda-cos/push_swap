cc5ec2b38c8b372cd913a6bef9b0cf2769c97022
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 17:53:37 +0000

    Add stack rotation functions for push_swap

--- push_swap.h
+++ push_swap.h
@@ -6,5 +6,9 @@
 
 void	exit_with_error(t_list **stack_a, t_list **stack_b);
 int		is_valid_integer(const char *str, long long *num);
+void	rotate_stack_up(t_list **stack);
+void	rotate_a(t_list **stack_a);
+void	rotate_b(t_list **stack_b);
+void	rotate_a_b(t_list **stack_a, t_list **stack_b);
 
 #endif
