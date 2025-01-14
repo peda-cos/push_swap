9017aa4c767567578e237d26b61de7a42a214cd9
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 20:05:35 +0000

    Add push and rotate operations for stack management

--- push_operations.c
+++ push_operations.c
@@ -23,3 +23,41 @@ void	push_to_b(t_list **stack_a, t_list **stack_b)
 	push_element(stack_b, stack_a);
 	ft_printf("pb\n");
 }
+
+void	push_back_to_a(t_list **stack_a, t_list **stack_b, int pushed)
+{
+	int	max_pos;
+
+	while (pushed > 0)
+	{
+		max_pos = find_max_position(*stack_b);
+		move_max_to_top(stack_b, max_pos);
+		push_to_a(stack_a, stack_b);
+		pushed--;
+	}
+}
+void	push_min_to_b(t_list **stack_a, t_list **stack_b)
+{
+	int min_pos;
+	int size;
+
+	size = ft_lstsize(*stack_a);
+	min_pos = find_min_position(*stack_a);
+	if (min_pos <= size / 2)
+	{
+		while (min_pos > 0)
+		{
+			rotate_a(stack_a);
+			min_pos--;
+		}
+	}
+	else
+	{
+		while (min_pos < size)
+		{
+			reverse_rotate_a(stack_a);
+			min_pos++;
+		}
+	}
+	push_to_b(stack_a, stack_b);
+}
--- push_swap.h
+++ push_swap.h
@@ -7,6 +7,8 @@
 void	push_element(t_list **dest_stack, t_list **src_stack);
 void	push_to_a(t_list **stack_a, t_list **stack_b);
 void	push_to_b(t_list **stack_a, t_list **stack_b);
+void	push_back_to_a(t_list **stack_a, t_list **stack_b, int pushed);
+void	push_min_to_b(t_list **stack_a, t_list **stack_b);
 
 void	swap_top_two_elements(t_list **stack);
 void	swap_a(t_list **stack_a);
@@ -28,5 +30,11 @@ void	sort_index_array(int *array, int size);
 void	assign_sorted_index(int *sorted_array, t_list *stack, int size);
 void	get_sort_index(t_list *stack);
 int		get_stack_median(t_list *stack);
+
+int		find_max_position(t_list *stack_b);
+void	move_max_to_top(t_list **stack_b, int max_pos);
+void	push_and_rotate(t_list **stack_a, t_list **stack_b);
+int		find_min_position(t_list *stack);
+
 
 #endif
