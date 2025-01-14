6bdd57f7c7bceb24136aaec1f6c18997696c9bf4
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 17:25:45 +0000

    exit_with_error add

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+#include "push_swap.h"
+
+void	exit_with_error(t_list **stack_a, t_list **stack_b)
+{
+	if (stack_a)
+		ft_lstclear(stack_a, free);
+	if (stack_b)
+		ft_lstclear(stack_b, free);
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
