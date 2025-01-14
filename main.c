c16052700f86b264a98dc68391d2e479c6592ade
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 17:29:08 +0000

    Added a check to verify if it is a number and if it is valid

--- main.c
+++ main.c
@@ -10,6 +10,30 @@ void	exit_with_error(t_list **stack_a, t_list **stack_b)
 	exit(1);
 }
 
+int	is_valid_integer(const char *str, long long *num)
+{
+	int			i;
+	long long	temp;
+
+	i = 0;
+	if (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		return (0);
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	temp = ft_atoi(str);
+	if (temp > INT_MAX || temp < INT_MIN)
+		return (0);
+	*num = temp;
+	return (1);
+}
 int	main(int argc, char **argv)
 {
 	(void)argc;
--- push_swap.h
+++ push_swap.h
@@ -2,6 +2,7 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+# include <limits.h>
 
 void	exit_with_error(t_list **stack_a, t_list **stack_b);
 
