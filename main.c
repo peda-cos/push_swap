40a82362965d54d612dc144aaa354d628b2d025d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-01-14 21:45:47 -0300

    Refactor main function and extract stack population logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 19:52:30 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/01/14 20:04:57 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/01/14 21:44:57 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,29 +48,35 @@ static int	contains_duplicate(t_list *stack, int num)
 	return (0);
 }
 
-int	main(int argc, char **argv)
+static void	populate_stack(int argc, char **argv, t_list **stack_a)
 {
-	t_list	*stack_a;
-	t_list	*stack_b;
 	long	num;
 	int		*content;
 
-	if (argc < 2)
-		return (0);
-	stack_a = NULL;
-	stack_b = NULL;
 	while (argc > 1)
 	{
 		argc--;
-		if (!is_valid_integer(argv[argc], &num) || contains_duplicate(stack_a,
+		if (!is_valid_integer(argv[argc], &num) || contains_duplicate(*stack_a,
 				num))
 			exit_with_error(NULL, NULL);
 		content = (int *)malloc(sizeof(int));
 		if (!content)
 			exit_with_error(NULL, NULL);
 		*content = num;
-		ft_lstadd_front(&stack_a, ft_lstnew(content));
+		ft_lstadd_front(stack_a, ft_lstnew(content));
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	if (argc < 2)
+		return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	populate_stack(argc, argv, &stack_a);
 	get_sort_index(stack_a);
 	if (!is_stack_sorted(stack_a))
 		sort_stack(&stack_a, &stack_b);
